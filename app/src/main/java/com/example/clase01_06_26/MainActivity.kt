77bdfbf0bcaf103f46c8acb7417b84593bf5e4ea
Emilio Chanes <emiliotorresemiliot@comunidad.unam.mx>
2026-06-01 21:01:09 -0600

    Ejercicio 1

--- app/src/main/java/com/example/clase01_06_26/MainActivity.kt
+++ app/src/main/java/com/example/clase01_06_26/MainActivity.kt
@@ -5,16 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.clase01_06_26.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val path: String = filesDir.absolutePath
+
+        binding.tvwRuta.text = path
     }
 }
